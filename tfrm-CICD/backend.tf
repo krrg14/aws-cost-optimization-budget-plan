bb411c0d20d4c7b2c005c6fa769ae1ed253db15c
krrg14 <krrakshithg@gmail.com>
2026-03-11 18:38:45 +0000

    updated all files

--- tfrm-CICD/backend.tf
+++ tfrm-CICD/backend.tf
@@ -1,9 +1,7 @@
-/*
-terraform {
+terraform { 
     backend "s3" {
         bucket = "terraform-backend-bucket-statefile-backup"
         key    =  "vpc/terraform.tfstate"
         region = "ap-south-1"
     }
 }
-*/
